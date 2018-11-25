1b7d97ac18406b0425aa9b6273650faab2a3e8e3
VishnuSwaminathan <vishcodes@gmail.com>
2018-11-24 20:48:49 -0500

    firstCommit

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import API from '../utils/api';
+import API from '../utils/API';
 import Search from './Search';
 import Articles from './Articles';
 import Saved from './Saved';
